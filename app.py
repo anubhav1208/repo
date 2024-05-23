f02879b06f57daba98d1cefb27186a01468b59b9
anubhav1208 <49609794+anubhav1208@users.noreply.github.com>
2024-05-23 13:28:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import streamlit as st
+import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings , OpenAIEmbeddings
 #from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+#from langchain.vectorstores import faiss
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.chat_models import ChatOpenAI
 from htmlTemplates import css, bot_template, user_template
 from langchain_community.llms import HuggingFaceHub
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+#from dotenv import load_dotenv
+
+
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -33,6 +44,7 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     #embeddings = OpenAIEmbeddings()
+    #embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
@@ -41,6 +53,7 @@ def get_vectorstore(text_chunks):
 def get_conversation_chain(vectorstore):
     #llm = ChatOpenAI()
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    #llm = ChatGoogleGenerativeAI(model = "gemini-pro", temperature = 0.3)
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -51,10 +64,13 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
-
 def handle_userinput(user_question):
+    if not st.session_state.conversation:
+        st.write("Please upload and process documents before asking a question.")
+        return
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
+
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
@@ -67,6 +83,9 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
+    os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    #genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
     st.set_page_config(page_title="Document Answering Bot",
                        page_icon="EY logo black.png")
     st.write(css, unsafe_allow_html=True)
@@ -108,3 +127,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
